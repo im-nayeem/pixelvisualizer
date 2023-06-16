bb8970db09cadf13def80ad78e62b50c07d7b679
Nayeem Hossain <nayeem.cse.cou@gmail.com>
2023-06-16 14:58:27 +0600

    update package - fix bug

--- build/lib/pixelvisualizer/drawer.py
+++ build/lib/pixelvisualizer/drawer.py
@@ -9,7 +9,7 @@ def draw_bresenhams_line(plotter, x1, y1, x2, y2, color = (0, 0, 0)):
     dx = x2 - x1
     dy = y2 - y1
 
-    if(dx <= 0 or dy < 0):
+    if(dx <= 0 or dy <= 0):
         draw_DDA_line(plotter, x1, y1, x2, y2, color)
         return
     
